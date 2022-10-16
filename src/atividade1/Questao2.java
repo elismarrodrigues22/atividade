bcf0ce3daf585aaa984b34ad6107db2bb485c089
elismar <infotec.elismar@gmail.com>
2022-10-16 19:17:00 -0300

    antecessor e sucessor

---
+++ src/atividade1/Questao2.java
@@ -0,0 +1,22 @@
+package atividade1;
+import java.util.Scanner;
+public class Questao2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    int numero,antecessor,sucessor;
+
+    System.out.println(" Informe um número ");
+    numero = scan.nextInt();
+
+    antecessor = numero - 1;
+    sucessor = numero + 1;
+
+    System.out.println(" O número informado fi : " + numero);
+    System.out.println(" O antecessor é : " + antecessor);
+    System.out.println(" O Sucessor é " + sucessor);
+
+    scan.close();
+    }
+
+    
+}
