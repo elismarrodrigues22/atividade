9628034909d0b20582dea94df6f97780d8c29ccd
elismar <infotec.elismar@gmail.com>
2022-10-24 07:57:25 -0300

    Alteração Questao04

--- src/atividade3/questao4.java
+++ src/atividade3/questao4.java
@@ -7,30 +7,30 @@ public class questao4 {
         String palavraDigitada = "";
         String palavra = "JAVA-2022";    
         int qtdTentativas = 4;
-    //OBSERVAÇÃO : TODA VEZ QUE REPETE O SEGUNDO LOOP A VARIÁVEL palavraDigitada FICA SALVO 
-      //  O QUE FOI DIGITADO ANTES E NÃO PARA PARA PERGUNTAR NOVAMENTE JÁ IMPRIME DIRETAMENTE
-    
+
         System.out.println("TESTE DE DIGITAÇÃO ");   
     
           
         
-       // System.out.println("Digite a palavra JAVA-2022 ");
-      //  palavraDigitada = scan.nextLine();
-     //   while(opcao != 0  || qtdTentativas < 1){
-            System.out.println("Digite a palavra JAVA-2022 ");
-            palavraDigitada = scan.nextLine();
-        if(palavraDigitada.equals(palavra)){
-            System.out.println("Palavra Correta");
-        }else{
-            System.out.println("Palavra Errada!");
-            qtdTentativas --;
-            System.out.println("Você tem " + qtdTentativas + "Tentativas restantes");
+        System.out.println("Digite a palavra JAVA-2022 ");
+        palavraDigitada = scan.nextLine();
+        switch(palavraDigitada){
+                    case "JAVA-2022":
+                    System.out.println("Palavra Correta");
+                    break;
+
+                    default:
+                    System.out.println("Palavra Incorreta Tente Novamente");
+                    while(qtdTentativas > 0 || palavraDigitada.equals(palavra)){
+                        System.out.println(" Quantidade de tentativas " + qtdTentativas);
+                        System.out.println("Digite a palavra JAVA-2022 ");
+                        palavraDigitada = scan.nextLine();
+                        qtdTentativas --;
+                       
+                    }
         }
-      //  System.out.println("Informe 1 pra continuar ou zero 0 pra sair");
-      //  opcao = scan.nextInt();
-       
-        
- //      }
+
+     
 
     }
     
