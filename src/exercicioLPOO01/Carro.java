6f20cc43c86a3a8d58510394a461bb14b464c906
elismar <infotec.elismar@gmail.com>
2022-11-02 23:49:32 -0300

    Criação Exercicio CarroPOO01

--- src/exercicioLPOO01/Carro.java
+++ src/exercicioLPOO01/Carro.java
@@ -5,10 +5,51 @@ import java.util.Scanner;
 public class Carro {
     private String marca;
     private String modelo;
-    private int velocidade;
-    Scanner scan = new Scanner(System.in);
+    private int velocidadeAtual;
+   
 
-    public Carro(){         
+    public Carro(String marca, String modelo){       
+        this.marca = marca;
+        this.modelo = modelo;  
+        velocidadeAtual = 0;
+    }
+
+    public void acelerar(int valor){
+        if((velocidadeAtual >=0 && velocidadeAtual <=200) && (velocidadeAtual+valor <= 200)){
+        System.out.println(" Acelerando ");
+        velocidadeAtual += valor;
+        System.out.println(" Velocidade Atual " + velocidadeAtual);
+    }else{
+        System.out.println(" A velocidade máxima do veículo é 200 km por hora");        
+        int contador = 200 - velocidadeAtual;
+        velocidadeAtual += contador;
+        System.out.println(" Velocidade Atual " + velocidadeAtual);
+    }
+    
+    }
+
+    public void frear(int valor){
+        velocidadeAtual -= valor;
+        if(velocidadeAtual <= 200 && velocidadeAtual > 0 ){
+            System.out.println(" Freando ");
+            System.out.println(" Velocidade Atual " + velocidadeAtual);
+        }
+            
+           if(velocidadeAtual < 0 ){
+                velocidadeAtual = 0;
+                System.out.println(" Velocidade Atual " + velocidadeAtual);
+            }
+            
+        }
+    
+
+    @Override
+    public String toString() {
+        return "Carro [marca=" + marca + ", modelo=" + modelo + ", velocidadeAtual=" + velocidadeAtual + "]";
+    }
+
+    public void printStatus(){
+        System.out.println(toString());
     }
   
 
--- src/exercicioLPOO01/questao01.java
+++ src/exercicioLPOO01/questao01.java
@@ -2,10 +2,14 @@ package exercicioLPOO01;
 import java.util.Scanner;
 public class questao01 {
     public static void main(String[] args) {
-        Carro carro1 = new Carro();
+        Carro carro = new Carro(" FIAT ", " PALIO ");
         
-        
-
-        
+        carro.printStatus();
+        carro.acelerar(20);        
+        carro.acelerar(50);
+        carro.acelerar(200);        
+        carro.frear(100);
+        carro.frear(150);
+        carro.printStatus();
     }
 }
